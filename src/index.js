89a156ab64f7f97b489020f6b26e68af62bd77c7
codeWithEdwin <social@kinloch.me>
2019-05-29 14:58:48 -0500

    Made folder changes.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import CardList from './CardList';
+import * as serviceWorker from './serviceWorker';
+import 'tachyons'
+import { Robots } from './Robots';
+
+ReactDOM.render(
+  <CardList Robots={Robots} />
+, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
